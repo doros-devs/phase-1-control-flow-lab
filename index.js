534e6c064918bd602e25b52c8bf053662250f60c
terenceouma <doros.devs@gmail.com>
2024-07-03 14:39:01 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let cost;
+
+  if (distance <= 400) {
+    cost = "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    cost = "That will be twenty bucks.";
+  } else if (distance > 2000 && distance <= 2500) {
+    cost = "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    cost = "No can do.";
+  }
+  return cost;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let message;
+
+    switch (tip) {
+      case "generous":
+        message = "Thank you so much."
+        break;
+      case "not as generous":
+        message = "Thank you."
+        break;
+    
+      default:
+        message = "Bye."
+        break;
+    }
+    
+    return message;
+    }
